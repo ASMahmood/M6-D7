ec939e48b21c61501b5b1e6b4cb6896460c4c26a
Abdul Samad Mahmood <72197815+ASMahmood@users.noreply.github.com>
2021-01-20 15:14:20 +0000

    ugly search

--- src/utilities/model.js
+++ src/utilities/model.js
@@ -85,16 +85,85 @@ class Model {
 
   //GET ALL METHOD
   async findOne(search) {
-    if (!search || Object.values(search).length === 0) {
+    if (
+      !search ||
+      (Object.values(search).length === 0 && this.name !== "articles")
+    ) {
       //BASIC GET ALL
+      console.log("1");
       const query = `SELECT * FROM ${this.name}`;
       const response = await this.run(query);
       return response.rows;
-    } else {
+    } else if (!search.query && this.name !== "articles") {
+      console.log("2");
       const entries = Object.entries(search);
       const query = `SELECT * FROM ${this.name} WHERE ${entries
         .map((entry) => `${entry[0]}='${entry[1]}'`)
         .join(" AND ")}`;
+      const response = await this.run(query);
+      return response.rows;
+    } else if (Object.values(search).length >= 1 && !search.query) {
+      console.log("3");
+      const entries = Object.entries(search);
+      const query = `SELECT 
+      articles.id, 
+      articles.headline, 
+      articles.subhead, 
+      articles.content, 
+      articles.cover, 
+      authors.id AS authorid,
+      authors.name AS author_name, 
+      authors.lastname AS author_lastname, 
+      authors.imgurl AS author_img,
+      categories.name AS category,
+      categories.imgurl AS category_img
+      FROM articles
+      INNER JOIN authors ON articles.authorid=authors.id
+      INNER JOIN categories ON articles.categoryid=categories.id
+      WHERE ${entries.map((entry) => `${entry[0]}='${entry[1]}'`).join(" AND ")}
+      `;
+      const response = await this.run(query);
+      return response.rows;
+    } else if (Object.values(search).length >= 1 && search.query) {
+      console.log("4");
+      const query = `SELECT 
+      articles.id, 
+      articles.headline, 
+      articles.subhead, 
+      articles.content, 
+      articles.cover, 
+      authors.id AS authorid,
+      authors.name AS author_name, 
+      authors.lastname AS author_lastname, 
+      authors.imgurl AS author_img,
+      categories.name AS category,
+      categories.imgurl AS category_img
+      FROM articles
+      INNER JOIN authors ON articles.authorid=authors.id
+      INNER JOIN categories ON articles.categoryid=categories.id
+      WHERE LOWER(articles.headline) LIKE LOWER('%${search.query}%') 
+      OR LOWER(articles.content) LIKE LOWER('%${search.query}%')
+      `;
+      const response = await this.run(query);
+      return response.rows;
+    } else {
+      const query = `SELECT 
+      articles.id, 
+      articles.headline, 
+      articles.subhead, 
+      articles.content, 
+      articles.cover, 
+      authors.id AS authorid,
+      authors.name AS author_name, 
+      authors.lastname AS author_lastname, 
+      authors.imgurl AS author_img,
+      categories.name AS category,
+      categories.imgurl AS category_img
+      FROM articles
+      INNER JOIN authors ON articles.authorid=authors.id
+      INNER JOIN categories ON articles.categoryid=categories.id
+      
+      `;
       const response = await this.run(query);
       return response.rows;
     }
